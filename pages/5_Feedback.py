d5a31120d41a0056f2a6253f075dfa8b60338736
Emmanuel F. Werr <47487994+emmanuelfwerr@users.noreply.github.com>
2023-05-26 21:05:51 +0200

    Final Update Feedback Form

--- pages/5_Feedback.py
+++ pages/5_Feedback.py
@@ -9,7 +9,27 @@ st.set_page_config(
 add_logo()
 sidebar()
 
-st.write("page 5 feedback")
+st.header("📨 Help Us Improve With Your Feedback!")
+st.write("We will get back to you personally as soon as we fix your bug or add the feature you have requested")
+
+contact_form = """
+<form action="https://formsubmit.co/spotipy.nx@gmail.com" method="POST">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
+
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
 
 
 # ---*--- Streamlit WebApp Footer ---*---
